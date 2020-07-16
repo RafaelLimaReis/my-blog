91c98e00400f7fba0aeb1953c332edcbbebdcbce
rafael lima <rafaellimareis01@gmail.com>
2020-07-16 15:06:44 -0300

    Listagem de posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,20 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import PostItem from '../components/PostItem';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home</h1>
+    <PostItem 
+        slug='/about/'
+        background='red'
+        category='Misc'
+        date='30 de julho de 2020'
+        timeToRead='5'
+        title='Consectetur adipisicing'
+        description='Elit labore non sint tempor quis.'
+    />
   </Layout>
 )
 
