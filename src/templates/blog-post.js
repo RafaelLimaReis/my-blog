e0ccfaa805faafcb1440da57f33add03309ef77d
rafael lima <rafaellimareis01@gmail.com>
2020-07-20 15:02:24 -0300

    config para comentarios

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby';
 
 import * as S from '../components/Post/styled';
 import RecommendedPosts from '../components/RecommendedPosts';
+import Comments from '../components/Comments';
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
 
@@ -23,6 +24,7 @@ const BlogPost = ({ data, pageContext }) => {
                 <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
             </S.MainContent>
             <RecommendedPosts next={next} previous={previous}/>
+            <Comments url={post.fields.slug} title={post.frontmatter.title}/>
         </Layout>
     );
 };
@@ -30,13 +32,16 @@ const BlogPost = ({ data, pageContext }) => {
 export const query = graphql`
     query Post($slug: String!) {
         markdownRemark(fields: {slug: {eq: $slug}}) {
-        frontmatter {
-            title
-            description
-            date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
-        }
-        timeToRead
-        html
+            fields {
+                slug
+            }
+            frontmatter {
+                title
+                description
+                date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+            }
+            timeToRead
+            html
         }
     }
 `;
